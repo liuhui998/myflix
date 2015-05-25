a397551ae036427aa0a9accb34518f58bdf091ff
peter.liu <liuhui998@gmail.com>
2015-05-25 09:49:11 +0800

    add video model's seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Video.destroy_all
+
+video_desc =  <<-EOM
+  Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.
+EOM
+Video.create(title: "Family Guy", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/family_guy.jpg")
+Video.create(title: "Futurama", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/futurama.jpg")
+Video.create(title: "Monk", description: video_desc, large_cover_image_url: "/tmp/monk_large.jpg", small_cover_image_url: "/tmp/monk.jpg")
+Video.create(title: "South Park", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/south_park.jpg")
