623f9b3618490cce6fe834a4d2d703b57e34954b
peter.liu <liuhui998@gmail.com>
2015-05-25 09:38:15 +0800

    create video model and run rake db:migrate
    rails g model video title description:text small_cover_image_url large_cover_image_url

---
+++ db/migrate/20150525013723_create_videos.rb
@@ -0,0 +1,12 @@
+class CreateVideos < ActiveRecord::Migration
+  def change
+    create_table :videos do |t|
+      t.string :title
+      t.text :description
+      t.string :small_cover_image_url
+      t.string :large_cover_image_url
+
+      t.timestamps
+    end
+  end
+end
