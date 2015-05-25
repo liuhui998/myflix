44f321cd4efa2d4135a407c2bfa097db2c6428cd
peter.liu <liuhui998@gmail.com>
2015-05-25 10:11:29 +0800

    add /home and videos#show routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Myflix::Application.routes.draw do
-  get 'videos/index'
+  get 'videos', to: 'videos#index', as: "videos_path"
+  get 'videos/:id',to: 'videos#show', as: "video"
+  get '/home', to: 'videos#index', as: "home_path"
 
   get 'ui(/:action)', controller: 'ui'
 end
