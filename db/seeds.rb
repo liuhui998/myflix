bb668b66e3a806baacd4c02ff1265940c89e2643
peter.liu <liuhui998@gmail.com>
2015-05-25 10:48:18 +0800

    create new seed data that support categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,11 +7,30 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Video.destroy_all
+Category.destroy_all
 
 video_desc =  <<-EOM
   Pizza boy Philip J. Fry awakens in the 31st century after 1,000 years of cryogenic preservation in this animated series. After he gets a job at an interplanetary delivery service, Fry embarks on ridiculous escapades to make sense of his predicament.
 EOM
-Video.create(title: "Family Guy", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/family_guy.jpg")
-Video.create(title: "Futurama", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/futurama.jpg")
-Video.create(title: "Monk", description: video_desc, large_cover_image_url: "/tmp/monk_large.jpg", small_cover_image_url: "/tmp/monk.jpg")
-Video.create(title: "South Park", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/south_park.jpg")
+
+videos = [
+  {title: "Family Guy", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/family_guy.jpg"},
+  {title: "Futurama", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/futurama.jpg"},
+  {title: "Futurama", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/futurama.jpg"},
+  {title: "Monk", description: video_desc, large_cover_image_url: "/tmp/monk_large.jpg", small_cover_image_url: "/tmp/monk.jpg"},
+  {title: "South Park", description: video_desc, large_cover_image_url: "http://dummyimage.com/665x375/000000/00a2ff", small_cover_image_url: "/tmp/south_park.jpg"}
+]
+
+categories = [
+  'TV Commedies',
+  'TV Dramas',
+  'Reality TV'
+]
+
+categories.each {
+  |cat_name|
+  category = Category.create(name: cat_name)
+  12.times {
+    category.videos.create(videos.sample)
+  }
+}
