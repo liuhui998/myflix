37f26e70ad0f06227c5ba1b2ebec2815d56b5b50
peter.liu <liuhui998@gmail.com>
2015-06-03 17:03:18 +0800

    completed "Assignment: Test Validations"

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -12,4 +12,17 @@ describe Video do
     gump1 = story.videos.create(title: "Forrest Gump", description: "Forrest Gump's story!")
     expect(gump1.category).to eq story
   end
+
+  it "does not save a video without title" do
+    video = Video.create(description: "Your story")
+    video.save
+    expect(Video.count).to eq 0
+  end
+
+  it "doest not save a video without description" do
+    video = Video.create(title: "A Stroy")
+    video.save
+    expect(Video.count).to eq 0
+  end
+
 end
