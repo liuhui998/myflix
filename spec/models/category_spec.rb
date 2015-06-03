24b10b9405df921e2fc692f0ea0a9f3e2318e8e4
peter.liu <liuhui998@gmail.com>
2015-06-03 16:46:30 +0800

    completed: "Assignment: Test the category model and associations"

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Category do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "save itself" do
+    category = Category.create(name: "Story")
+    category.save
+    expect(Category.first).to eq category
+  end
+
+  it "has many videos" do
+    story = Category.create(name: "Story")
+    gump1 = Video.create(title: "Forrest Gump 1", description: "Forrest Gump's story", category: story)
+    gump2 = Video.create(title: "Forrest Gump 2", description: "Forrest Gump's story", category: story)
+    
+    expect(story.videos).to include(gump1,gump2)
+  end
 end
--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -6,4 +6,10 @@ describe Video do
     video.save
     expect(Video.first).to eq video
   end
+
+  it "belongs to category" do
+    story = Category.create(name: "Story")
+    gump1 = story.videos.create(title: "Forrest Gump", description: "Forrest Gump's story!")
+    expect(gump1.category).to eq story
+  end
 end
