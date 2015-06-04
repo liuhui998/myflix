3a528ff4d3ebd2cf9db788721846b4b7720a2160
peter.liu <liuhui998@gmail.com>
2015-06-04 11:25:56 +0800

    using shoulda test validates_presence_of

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -4,23 +4,8 @@ require 'spec_helper'
 RSpec.describe Video, type: :model do
   
   it { should belong_to(:category) }
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:description) }
 
-  it "belongs to category" do
-    story = Category.create(name: "Story")
-    gump1 = story.videos.create(title: "Forrest Gump", description: "Forrest Gump's story!")
-    expect(gump1.category).to eq story
-  end
-
-  it "does not save a video without title" do
-    video = Video.create(description: "Your story")
-    video.save
-    expect(Video.count).to eq 0
-  end
-
-  it "doest not save a video without description" do
-    video = Video.create(title: "A Stroy")
-    video.save
-    expect(Video.count).to eq 0
-  end
 
 end
