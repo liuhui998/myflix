0ed1e1d9458d966516e6ae632088fae19c18dacb
peter.liu <liuhui998@gmail.com>
2015-06-16 09:12:22 +0800

    add Video.search_by_title spec test skeleton

--- spec/models/video_spec.rb
+++ spec/models/video_spec.rb
@@ -7,5 +7,11 @@ RSpec.describe Video, type: :model do
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:description) }
 
+  describe "search_by _title" do
+    it "returns an empty array if there is no match"
+    it "returns an array of one video for an exact match"
+    it "returns an array of one video for a partial match"
+    it "returns an array of all matchs ordered by created_at"    
+  end
 
 end
