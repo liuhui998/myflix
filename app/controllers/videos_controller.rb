c1256a7b1c11c071a81f4604945e59a06910f930
peter.liu <liuhui998@gmail.com>
2015-06-17 23:22:32 +0800

    Implement search in the UI

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -7,4 +7,9 @@ class VideosController < ApplicationController
   def show
     @video = Video.find(params[:id])
   end
+  
+  def search
+    @results = Video.search_by_title(params[:search_term])
+  end
+
 end
