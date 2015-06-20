d9351f4cf4a1aacd032875ff3617f9de074d037b
peter.liu <liuhui998@gmail.com>
2015-06-20 12:34:49 +0800

    use delete method to sign out

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Myflix::Application.routes.draw do
   get '/home', to: 'videos#index', as: "home"
   get 'register', to: "users#new"
   get 'sign_in' , to: "sessions#new"
-  get 'sign_out', to: "sessions#destroy"
+  delete 'sign_out', to: "sessions#destroy"
 
   resources :users, only: [:create]
   resources :sessions, only: [:create]
